d9ca39ccb7796e4b643dbcdc82b68c2abc242036
SamSam <168691193+samtesito@users.noreply.github.com>
2024-11-21 11:48:40 -0400

    Cambios temporales para mi archivo

--- ProyectoPoo/src/controladora/ProyectoPoo.java
+++ ProyectoPoo/src/controladora/ProyectoPoo.java
@@ -1,5 +1,5 @@
 package controladora;
-
+import java.util.ArrayList;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -16,7 +16,14 @@ public class ProyectoPoo{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        String[] mantequilla = {"sexo", "coito", "penetracion", "singaera"};
+        recursos.globalAccess.listaDePreguntas = new ArrayList<controladora.Pregunta>();
+        recursos.globalAccess.listaDePreguntas.add(new controladora.Pregunta("Sexoooo",mantequilla,2,350.15));
+        recursos.globalAccess.listaDePreguntas.add(new controladora.Pregunta("Gaitas",mantequilla,1,350.15));
+        recursos.globalAccess.listaDePreguntas.add(new controladora.Pregunta("Periqueo",mantequilla,3,350.15));
+        recursos.globalAccess.listaDePreguntas.add(new controladora.Pregunta("Gorras",mantequilla,0,350.15));
+        vistas.interfazAdmin NuevaVentana = new vistas.interfazAdmin();
+        NuevaVentana.setVisible(true);
     }
     
 }
